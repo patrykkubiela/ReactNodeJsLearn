318aff206bb4130d3ca9b96638bdbc37da81667c
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-07-09 19:00:34 +0200

    create home page

--- monodotodo/src/App.tsx
+++ monodotodo/src/App.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import tw from 'twin.macro';
+import { HomePage } from './app/containers/HomePage';
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `}
+`;
+
 function App() {
-  return (
-    <div className="App">
-      Hello world!!!
-    </div>
-  );
+  return <PageContainer>
+    <HomePage/>
+  </PageContainer>;
 }
 
 export default App;
