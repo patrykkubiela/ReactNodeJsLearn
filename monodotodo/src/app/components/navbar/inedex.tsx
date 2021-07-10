65261ed96b3b11727fb3366d58231c9681ccf97c
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-07-10 11:21:30 +0200

    end navbar with responsivnes for mobile version (burger-menu)

--- monodotodo/src/app/components/navbar/inedex.tsx
+++ monodotodo/src/app/components/navbar/inedex.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Logo } from "../logo";
+import { NavItems } from "./navitems";
 
 const NavbarContainer = styled.div`
     min-height: 68px;
     ${tw`
         w-full
-        max-w-2xl
+        min-w-full
+        max-w-screen-2xl
         flex
         flex-row
         items-center
@@ -35,5 +37,6 @@ export function Navbar() {
         <LogoContainer>
             <Logo/>
         </LogoContainer>
+        <NavItems/>
     </NavbarContainer>
 }
